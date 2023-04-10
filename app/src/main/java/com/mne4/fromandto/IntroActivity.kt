54fd83c3dddce9d37039698cf014c5aebbe2637d
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-10 22:43:56 +0300

    API

--- app/src/main/java/com/mne4/fromandto/IntroActivity.kt
+++ app/src/main/java/com/mne4/fromandto/IntroActivity.kt
@@ -3,7 +3,7 @@ package com.mne4.fromandto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class IntroAct1 : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_act1)
