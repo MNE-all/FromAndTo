05355a977581082053de487e2c2c955977a23ea2
MNE4 <monashovnikolaye@gmail.com>
2023-04-10 19:59:52 +0300

    binding

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mne4.fromandto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mne4.fromandto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
