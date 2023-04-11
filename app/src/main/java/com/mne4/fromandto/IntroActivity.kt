5a835875064b5a16d4d6a38e24a55e321f9d1f92
MNE4 <monashovnikolaye@gmail.com>
2023-04-12 01:01:32 +0300

    activitys refactoring

--- app/src/main/java/com/mne4/fromandto/IntroActivity.kt
+++ app/src/main/java/com/mne4/fromandto/IntroActivity.kt
@@ -1,9 +1,11 @@
 package com.mne4.fromandto
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.asLiveData
@@ -21,77 +23,80 @@ import java.time.LocalDate
 class IntroActivity : AppCompatActivity() {
     lateinit var binding: ActivityIntroLoadScreenBinding
     private var viewModel = ViewModel()
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroLoadScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var db = MainDB.getDB(this)
+
+        checkLocalDb()
 
 
-/*
-        var date = LocalDate.parse("2003-10-27")
-        var users= User(
-            "Николаев",
-            "Вячеслав",
-            "Мужской",
-            "$date",
-            "slavanik@gmail.com",
-            "123456",
-            "123456",
-            "89645843532",
-            true,
-            5.0,
-            null,
-            null,
-            null
-        )
-        viewModel.postNewUser(users)
-        viewModel.dataModel.ApiReturnUser.observe(this) {
-            var user: com.mne4.fromandto.db.User = com.mne4.fromandto.db.User(
-                null,
-                "${it.id_user}",
-                "${it.surname}",
-                "${it.name}",
-                "${it.birthday}",
-                "${it.gender}",
-                "${it.phone}",
-                false
-            )
-            CoroutineScope(Dispatchers.IO).launch {
-                db.getDao().insertUser(user)
-            }
+
+
+    }
+
+    private fun firstIntroPage() {
+        setContentView(R.layout.activity_intro_first_page)
+        var button = findViewById<Button>(R.id.buttonIntroFirstNext)
+        button.setOnClickListener {
+            notificationPage()
         }
-        */
-        db.getDao().getAllUser().asLiveData().observe(this) {
-            if (it.isEmpty()) {
-                Log.d("userEmpty", "null")
-            } else {
-                Log.d("userEmpty2", "not null")
-            }
+    }
+
+    private fun notificationPage() {
+        setContentView(R.layout.activity_intro_notification_page)
+        // TODO Запросить разрешение на уведомление
+        var buttonYes = findViewById<Button>(R.id.buttonNotificationOn)
+        var buttonNo = findViewById<Button>(R.id.buttonNotificationOff)
+        buttonYes.setOnClickListener {
+            // TODO Пользователь разрешает уведомления
+            loginOrRegister()
         }
+        buttonNo.setOnClickListener {
+            // TODO Пользователь запрещает уведомления
+            loginOrRegister()
+        }
+    }
 
+    private fun loginOrRegister() {
+        setContentView(R.layout.activity_intro_login_register)
+        var buttonLogin = findViewById<Button>(R.id.buttonLoginLoginRegister)
+        var buttonRegister = findViewById<Button>(R.id.buttonRegisterLoginRegister)
 
-/*
-    //        var password = viewModel.postSMS("79657683902")
-    //        Log.d("passwordSms","$password")
-            var date = LocalDate.parse("2003-10-27")
-            var users= User(
-                "Николаев",
-                "Вячеслав",
-                "Мужской",
-                "$date",
-                "slavanik@gmail.com",
-                "123456",
-                "123456",
-                "89645843532",
-                true,
-                5.0,
-                null,
-                null,
-                null
-            )
-           */
+        buttonLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        buttonRegister.setOnClickListener {
 
         }
     }
+
+    private fun checkLocalDb() {
+        var db = MainDB.getDB(this)
+        db.getDao().getAllUser().asLiveData().observe(this) {
+            var isInAccount = false
+            if (it.isEmpty()) {
+                Log.d("isUserEmpty", "true")
+                firstIntroPage()
+            }
+            else
+            {
+                for (user in it) {
+                    if (user.isInAcc) {
+                        isInAccount = true
+                    }
+                }
+                Log.d("isUserEmpty", "false")
+                if (isInAccount) {
+                    var intent = Intent(this, WelcomeActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    // TODO в alertDialog предлагать использовать один из аккаунтов из локальной базы данных + авторизовать пользователя
+                }
+            }
+        }
+    }
+}
