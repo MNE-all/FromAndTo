637c0fb6fb4972695949832cdfc628797a35eafe
MNE4 <monashovnikolaye@gmail.com>
2023-04-11 09:35:05 +0300

    refactoring application architecture

--- app/src/main/java/com/mne4/fromandto/IntroActivity.kt
+++ app/src/main/java/com/mne4/fromandto/IntroActivity.kt
@@ -2,10 +2,13 @@ package com.mne4.fromandto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mne4.fromandto.databinding.ActivityIntroLoadScreenBinding
 
 class IntroActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIntroLoadScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro_act1)
+        binding = ActivityIntroLoadScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
