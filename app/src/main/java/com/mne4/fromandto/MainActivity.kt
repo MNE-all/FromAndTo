8b4bc43ef9436f81c6ec2938dcefc69ca41447bd
KlounFromRivia <106807009+KlounFromRivia@users.noreply.github.com>
2023-04-11 18:59:10 +0300

    Сделал RegisterActivity без логики (макет)

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.mne4.fromandto
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import androidx.lifecycle.asLiveData
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.mne4.fromandto.databinding.ActivityMainBinding
-import com.mne4.fromandto.db.MainDB
-import com.mne4.fromandto.db.User
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,8 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        binding.textView2.setSelected(true)
+//        binding.textView.setSelected(true)
 
-            /*var db = MainDB.getDB(this)
+        /*var db = MainDB.getDB(this)
 
         db.getDao().getAllUser().asLiveData().observe(this){
             binding.edMessage.text = ""
