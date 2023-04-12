45d89c14dc6786eb867e9f0524981e383d57d867
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-12 04:41:14 +0300

    Registration

--- app/src/main/java/com/mne4/fromandto/RegisterActivity.kt
+++ app/src/main/java/com/mne4/fromandto/RegisterActivity.kt
@@ -1,25 +1,29 @@
 package com.mne4.fromandto
 
+import android.annotation.SuppressLint
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import com.mne4.fromandto.databinding.ActivityIntroLoginRegisterBinding
+import com.mne4.fromandto.API.ViewModel
+
 
 class RegisterActivity : AppCompatActivity() {
-    lateinit var viewModel:viewModel()
+    lateinit var viewModel:ViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intro_act10)
 
-        viewModel=viewModel()
+        viewModel= ViewModel()
     }
 
+   @SuppressLint("MissingInflatedId")
    fun butReg(view: View){
 
         var Surname = findViewById<TextInputEditText>(R.id.surnameField).text
@@ -47,15 +51,27 @@ class RegisterActivity : AppCompatActivity() {
        var dialog:AlertDialog.Builder = AlertDialog.Builder(this)
        dialog.setTitle("Подтверждение номера")
        dialog.setMessage("Поддтвердите номер телефона. Введите пароль по смс!")
-       viewModel.
+       var passwordGenereted = viewModel.postSMS(Phone)
        var inflater: LayoutInflater = LayoutInflater.from(this)
-       var reg_windom = inflater.inflate(R.layout.intro_act10,null)
+       var reg_windom = inflater.inflate(R.layout.activity_sms_send_dialog,null)
+       var number:EditText = reg_windom.findViewById(R.id.editTextNumber)
+       var number2:EditText = reg_windom.findViewById(R.id.editTextNumber2)
+       var number3:EditText = reg_windom.findViewById(R.id.editTextNumber3)
+       var number4:EditText = reg_windom.findViewById(R.id.editTextNumber4)
        dialog.setView(reg_windom)
+
+
 
        dialog.setPositiveButton("Подтвердить",DialogInterface.OnClickListener{dialog: DialogInterface?, i->
 
+           var text:String
+           text = number.text.toString() + number2.text.toString() + number3.text.toString() + number4.text.toString()
 
-
+           if(text == passwordGenereted.toString()) {
+               Toast.makeText(this, "Номер подтвержден!", Toast.LENGTH_SHORT).show()
+           }else{
+               Toast.makeText(this, "Неправильно введенный пароль!", Toast.LENGTH_SHORT).show()
+           }
        })
 
 
