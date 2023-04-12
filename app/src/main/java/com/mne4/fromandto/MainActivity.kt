4380559b5c68971c6c094be099f1f3db649305eb
KlounFromRivia <106807009+KlounFromRivia@users.noreply.github.com>
2023-04-12 22:41:07 +0300

    Подправил парочку моментов (ошибки в дизайне register и заказов)

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -17,35 +17,5 @@ class MainActivity : AppCompatActivity() {
 //        binding.textView2.setSelected(true)
 //        binding.textView.setSelected(true)
 
-        /*var db = MainDB.getDB(this)
-
-        db.getDao().getAllUser().asLiveData().observe(this){
-            binding.edMessage.text = ""
-            it.forEach{
-                val text = "Id:${it.id} id_user:${it.id_user}\n" +
-                        "Surname: ${it.surname} Name: ${it.name}\n" +
-                        "birthday: ${it.birthday} phone: ${it.phone}\n" +
-                        "isInAcc: ${it.isInAcc} gender: ${it.gender}\n"
-                binding.edMessage.append(text)
-            }
-        }
-serfse
-        binding.button.setOnClickListener{
-            var user = User(
-                null,
-                "3453-34534-34534-535",
-                "Николаев",
-                "Вячеслав",
-                "27:10:2003",
-                "Мужской",
-                "89645843532",
-                true
-            )
-            CoroutineScope(Dispatchers.IO).launch {
-                db.getDao().insertUser(user)
-            }
-        }
-*/
-
     }
 }
