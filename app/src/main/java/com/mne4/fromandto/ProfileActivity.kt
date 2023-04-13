10d48246edf76e1c742215dfdec90dc785331b29
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-13 22:30:18 +0300

    Profile

--- app/src/main/java/com/mne4/fromandto/ProfileActivity.kt
+++ app/src/main/java/com/mne4/fromandto/ProfileActivity.kt
@@ -3,8 +3,14 @@ package com.mne4.fromandto
 import android.app.DatePickerDialog
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.mne4.fromandto.databinding.ActivityProfileBinding
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -13,18 +19,45 @@ import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
-    private lateinit var maxDate:LocalDate
-    private lateinit var minDate:LocalDate
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        maxDate = LocalDate.now()
-        minDate = maxDate.minusYears(100)
+        SpinnerGender()
+        DateDialog()
+    }
 
+    private fun updateText(calendar: Calendar){
+        val dateFormat = "dd-MM-yyyy"
+        var simple = SimpleDateFormat(dateFormat,Locale.UK)
+        binding.txtCalendar.text = simple.format(calendar.time)
+    }
 
+    private fun SpinnerGender(){
+
+        var genderList = arrayOf("Мужской","Женский")
+        var arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, genderList)
+        binding.spinnerGender.adapter = arrayAdapter
+        binding.spinnerGender.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Snackbar.make(view!!,"Вы выбрали пол: ${genderList[position]}",Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+
+    }
+    private fun DateDialog(){
         //TODO Календарь доработать
         var calendarBox = Calendar.getInstance()
 
@@ -48,15 +81,5 @@ class ProfileActivity : AppCompatActivity() {
                 calendarBox.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
-    }
-
-    private fun updateText(calendar: Calendar){
-        val dateFormat = "dd-MM-yyyy"
-        var simple = SimpleDateFormat(dateFormat,Locale.UK)
-        binding.txtCalendar.text = simple.format(calendar.time)
-
-
-
-
     }
 }
