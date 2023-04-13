22b9c44dc863e52fe8525a55ce38dc2508530060
Николай Монашов <monashovnikolaye@gmail.com>
2023-04-13 17:34:16 +0300

    search logic is ready

--- app/src/main/java/com/mne4/fromandto/IntroActivity.kt
+++ app/src/main/java/com/mne4/fromandto/IntroActivity.kt
@@ -1,14 +1,18 @@
 package com.mne4.fromandto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mne4.fromandto.databinding.ActivityIntroLoadScreenBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.MapKitFactory
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_load_screen)
+        // Initialize the library to load required native libraries.
+        // Warning! It's heavy operation
+        MapKitFactory.setApiKey("429ae64e-46c4-4b6a-aebe-e8ef49cbc0c5")
+        MapKitFactory.initialize(this)
 
         var intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.mne4.fromandto
 
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.PointF
 import android.location.Geocoder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Im
-import android.se.omapi.Session
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.UiThread
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputEditText
 import com.yandex.mapkit.Animation
-import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.RequestPoint
-import com.yandex.mapkit.RequestPointType
-import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.*
-import com.yandex.mapkit.geometry.Direction
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.layers.ObjectEvent
-import com.yandex.mapkit.location.Location
-import com.yandex.mapkit.location.LocationListener
-import com.yandex.mapkit.location.LocationStatus
 import com.yandex.mapkit.map.*
 import com.yandex.mapkit.map.Map
 import com.yandex.mapkit.mapview.MapView
-import com.yandex.mapkit.search.Response
-import com.yandex.mapkit.search.SearchFactory
-import com.yandex.mapkit.search.SearchManager
-import com.yandex.mapkit.search.SearchManagerType
-import com.yandex.mapkit.search.SearchOptions
+import com.yandex.mapkit.search.*
 import com.yandex.mapkit.user_location.UserLocationLayer
 import com.yandex.mapkit.user_location.UserLocationObjectListener
 import com.yandex.mapkit.user_location.UserLocationView
@@ -50,9 +26,8 @@ import com.yandex.runtime.image.ImageProvider
 import com.yandex.runtime.network.NetworkError
 import com.yandex.runtime.network.RemoteError
 import java.util.*
-import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjectListener,
+class MainActivity : AppCompatActivity(), UserLocationObjectListener,
     com.yandex.mapkit.search.Session.SearchListener, CameraListener {
     lateinit var mapView: MapView
     lateinit var cameraListener: CameraListener
@@ -72,9 +47,6 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
     lateinit var searchSession:com.yandex.mapkit.search.Session
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapKitFactory.setApiKey("429ae64e-46c4-4b6a-aebe-e8ef49cbc0c5")
-        MapKitFactory.initialize(this)
-
         setContentView(R.layout.activity_main)
 
         radioFrom = findViewById(R.id.radioFrom)
@@ -120,7 +92,7 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
         searchEdit.setOnEditorActionListener{
             v, actionId, event ->
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
-                sumbitQuery(searchEdit.text.toString())
+                submitQuery(searchEdit.text.toString())
             }
             false
         }
@@ -134,7 +106,7 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
         var imgId = resources.getIdentifier("baseline_location_on_24","drawable",this.packageName)
         imgPin.setImageResource(imgId)
         if(txtTo.text != "Откуда"){
-            PositionCamera(START_POSITION)
+            cameraPosition(START_POSITION)
         }
 
 
@@ -145,45 +117,65 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
         var imgId = resources.getIdentifier("baseline_location_off_24","drawable",this.packageName)
         imgPin.setImageResource(imgId)
         if(txtTo.text != "Куда"){
-            PositionCamera(END_POSITION)
+            cameraPosition(END_POSITION)
         }
 
 
     }
-    fun PositionCamera(position: Point){
+    fun cameraPosition(position: Point){
         mapView.map.move(CameraPosition(position, 17.0f, 0.0f, 0.0f),
             Animation(Animation.Type.SMOOTH, 1f), null
         )
     }
 
-            /*var db = MainDB.getDB(this)
 
-        db.getDao().getAllUser().asLiveData().observe(this){
-            binding.edMessage.text = ""
-            it.forEach{
-                val text = "Id:${it.id} id_user:${it.id_user}\n" +
-                        "Surname: ${it.surname} Name: ${it.name}\n" +
-                        "birthday: ${it.birthday} phone: ${it.phone}\n" +
-                        "isInAcc: ${it.isInAcc} gender: ${it.gender}\n"
-                binding.edMessage.append(text)
+    override fun onStop() {
+        mapView!!.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView!!.onStart()
+    }
+
+    override fun onSearchResponse(response: Response) {
+        val mapObjects = mapView!!.map.mapObjects
+        mapObjects.clear()
+        for (searchResult in response.collection.children) {
+            val resultLocation = searchResult.obj!!.geometry[0].point
+            if (resultLocation != null) {
+                mapObjects.addPlacemark(
+                    resultLocation,
+                    ImageProvider.fromResource(this, R.drawable.search_result)
+                )
             }
         }
-        binding.button.setOnClickListener{
-            var user = User(
-                null,
-                "3453-34534-34534-535",
-                "Николаев",
-                "Вячеслав",
-                "27:10:2003",
-                "Мужской",
-                "89645843532",
-                true
-            )
-            CoroutineScope(Dispatchers.IO).launch {
-                db.getDao().insertUser(user)
-            }
+    }
+
+    override fun onSearchError(error: Error) {
+        var errorMessage = "getString(R.string.unknown_error_message)"
+        if (error is RemoteError) {
+            errorMessage = "getString(R.string.remote_error_message)"
+        } else if (error is NetworkError) {
+            errorMessage = "getString(R.string.network_error_message)"
         }
-*/
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCameraPositionChanged(
+        map: Map,
+        cameraPosition: CameraPosition,
+        cameraUpdateReason: CameraUpdateReason,
+        finished: Boolean
+    ) {
+        if (finished) {
+            submitQuery(searchEdit!!.text.toString())
+        }
+    }
+
 
 
     fun onAddClick(view: View) {
@@ -203,17 +195,6 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-        MapKitFactory.getInstance().onStop()
-    }
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-        MapKitFactory.getInstance().onStart()
-    }
-
     private fun requestLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
@@ -229,24 +210,13 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
         }
     }
 
-
-    @UiThread
-    override fun onStart(p0: Map, p1: CameraPosition) {
-        Log.d("pos","Start!!!!!!!!!!!!!!!!!!!!!!!!")
-    }
-
-    @UiThread
-    override fun onCancel(p0: Map, p1: CameraPosition) {
-        Log.d("pos","Cansel!!!!!!!!!!!!!!!!!!!!!!!!")
-    }
-    @UiThread
-    override fun onFinish(p0: Map, p1: CameraPosition) {
-        Log.d("pos","Finish!!!!!!!!!!!!!!!!!!!!!!!!")
-    }
-
-
-    private fun sumbitQuery(query:String){
-        searchSession  = searchManager.submit(query,VisibleRegionUtils.toPolygon(mapView.map.visibleRegion), SearchOptions(), this)
+    private fun submitQuery(query: String) {
+        searchSession = searchManager.submit(
+            query,
+            VisibleRegionUtils.toPolygon(mapView.map.visibleRegion),
+            SearchOptions(),
+            this
+        )
     }
     override fun onObjectAdded(userLocation: UserLocationView) {
         locationMapKit.setAnchor(
@@ -270,40 +240,5 @@ class MainActivity : AppCompatActivity(), InertiaMoveListener, UserLocationObjec
     override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
 
     }
-
-    @SuppressLint("SuspiciousIndentation")
-    override fun onSearchResponse(response: Response) {
-     val mapObjects:MapObjectCollection = mapView.map.mapObjects
-        mapObjects.clear()
-        for(searchResult in response.collection.children){
-            val resultLocation = searchResult.obj!!.geometry[0].point!!
-            if(response!=null){
-                mapObjects.addPlacemark(resultLocation, ImageProvider.fromResource(this, R.drawable.ic_search))
-            }
-
-        }
-    }
-
-    override fun onSearchError(error:Error) {
-       var errorMessage = "Неизвестная проблема"
-        if(error is RemoteError){
-            errorMessage = "Беспроводная ошибка"
-        }else if(error is NetworkError){
-            errorMessage = "Проблема с интернетом"
-        }
-        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT)
-    }
-
-    override fun onCameraPositionChanged(
-        map: Map,
-        cameraPosition: CameraPosition,
-        cameraUpdate: CameraUpdateReason,
-        finish: Boolean
-    ) {
-        if(finish){
-            sumbitQuery(searchEdit.text.toString())
-        }
-    }
-
 
 }
