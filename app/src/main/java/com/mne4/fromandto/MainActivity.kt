2c100af48648a2953249d01d865c4716e0296820
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-27 02:06:02 +0300

    Data

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mne4.fromandto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.mne4.fromandto.Fragment.BarMenuFragment
 import com.mne4.fromandto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fragmentInstance(BarMenuFragment::class.java, R.layout.fragment_bar_menu);
 
+    }
+    fun fragmentInstance(f: Fragment, idHolder: Int) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(idHolder, f)
+            .commit()
     }
 
 
