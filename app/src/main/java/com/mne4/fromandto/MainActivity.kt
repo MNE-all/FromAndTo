2b4634cc29a9570ebe7f50613c1ea254677afbf9
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-27 16:51:09 +0300

    Data

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -1,29 +1,61 @@
 package com.mne4.fromandto
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.LinearLayout
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.mne4.fromandto.Fragment.BarMenuFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mne4.fromandto.databinding.ActivityMainBinding
-import com.yandex.mapkit.search.Line
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnSearch: com.google.android.material.floatingactionbutton.FloatingActionButton
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-
-        fragmentInstance(BarMenuFragment.newInstance(),R.id.framelayoutMenu);
-        var menuFragment = LayoutInflater.from(this).inflate(R.layout.fragment_bar_menu,null)
-        btnSearch = menuFragment.findViewById(R.id.createRequest)
-        btnSearch.setOnClickListener{
-            Toast.makeText(applicationContext, "Вы нажади искать!",Toast.LENGTH_SHORT).show()
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       binding.bottomNavigationViewMenu.setBackground(null)
+        binding.floatingActionButton.setOnClickListener{
+            Toast.makeText(applicationContext,"Вы нажали на кнопку создание запроса!",Toast.LENGTH_SHORT).show()
         }
+        binding.bottomNavigationViewMenu.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.butSearchBottomNavigation -> {
+                    Toast.makeText(
+                        applicationContext,
+                        "Вы нажали на кнопку поиска!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                R.id.butHelpBottomNavigation -> {
+                    Toast.makeText(
+                        applicationContext,
+                        "Вы нажали на кнопку помощи!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                R.id.butSettingBottomNavigation -> {
+                    Toast.makeText(
+                        applicationContext,
+                        "Вы нажали на кнопку настройки!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                R.id.butProfileBottomNavigation -> {
+                    Toast.makeText(
+                        applicationContext,
+                        "Вы нажали на кнопку профиля!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            true
+        }
+//        fragmentInstance(BarMenuFragment.newInstance(),R.id.framelayoutMenu);
+
     }
     fun fragmentInstance(f: Fragment, idHolder: Int) {
         supportFragmentManager
