0fdf2a0cf6ea5dc3f26a3a39a529afc93d88aa3b
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-27 03:23:29 +0300

    Data

--- app/src/main/java/com/mne4/fromandto/MainActivity.kt
+++ app/src/main/java/com/mne4/fromandto/MainActivity.kt
@@ -2,19 +2,27 @@ package com.mne4.fromandto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mne4.fromandto.Fragment.BarMenuFragment
 import com.mne4.fromandto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var btnSearch: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fragmentInstance(BarMenuFragment::class.java, R.layout.fragment_bar_menu);
 
+
+        fragmentInstance(BarMenuFragment.newInstance(),R.id.framelayoutMenu);
+        btnSearch = findViewById(R.id.btnSearch)
+        btnSearch.setOnClickListener{
+            Toast.makeText(applicationContext, "Вы нажади искать!",Toast.LENGTH_SHORT).show()
+        }
     }
     fun fragmentInstance(f: Fragment, idHolder: Int) {
         supportFragmentManager
--- app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
+++ app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
@@ -26,7 +26,7 @@ class WelcomeActivity : AppCompatActivity() {
 
     fun onPassengerClick(view: View) {
         intent.putExtra("UserStatus", "User")
-        startActivity(Intent(applicationContext, ProfileActivity::class.java))
+        startActivity(Intent(applicationContext, MainActivity::class.java))
     }
 
     fun onExit(view: View) {
