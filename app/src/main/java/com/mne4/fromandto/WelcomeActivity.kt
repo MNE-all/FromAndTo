f264fdf20887c1d4b77b5d78dc4d09c697a65051
MNE4 <monashovnikolaye@gmail.com>
2023-04-28 09:09:48 +0300

    change logo

--- app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
+++ app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.lifecycle.asLiveData
+import com.mne4.fromandto.Data.DataModel
 import com.mne4.fromandto.Data.Room.MainDB
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WelcomeActivity : AppCompatActivity() {
+    val viewModel: DataModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
@@ -30,13 +33,13 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     fun onExit(view: View) {
-        var db = MainDB.getDB(this)
-        db.getDao().getAllUser().asLiveData().observe(this) {
+        var localDB = viewModel.getLocalDB(this)
+        localDB.getDao().getAllUser().asLiveData().observe(this) {
             if (it.isNotEmpty()) {
                 for (user in it){
                     if (user.isInAcc) {
                         CoroutineScope(Dispatchers.IO).launch {
-                            db.getDao().updateUserisAcc(
+                            localDB.getDao().updateUserisAcc(
                                 "${user.id_user}",
                                false
                             )
