4117026de1535b9ebeabe49b3ba21209c96df3a0
MNE4 <monashovnikolaye@gmail.com>
2023-05-03 10:06:12 +0300

    WelcomeActivity hello user

--- app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
+++ app/src/main/java/com/mne4/fromandto/WelcomeActivity.kt
@@ -1,17 +1,18 @@
 package com.mne4.fromandto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.asLiveData
 import com.mne4.fromandto.Data.DataModel
-import com.mne4.fromandto.Data.Room.MainDB
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
 
 
 class WelcomeActivity : AppCompatActivity() {
@@ -19,33 +20,54 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        findViewById<TextView>(R.id.textViewExitAccount).setOnClickListener {
+            onExit()
+        }
+
+        viewModel.getLocalDB(this).getDao().getAllUser().asLiveData().observe(this) {
+            for (user in it) {
+                if (user.isInAcc) {
+                    viewModel.getCurrentUser(user.id_user)
+                    viewModel.ApiGetCurrentUser.observe(this) { userAPI ->
+                        val img = findViewById<ImageView>(R.id.imageViewAvatar)
+                        Picasso.get().load(userAPI.image_url)
+                            .placeholder(R.drawable.baseline_account_circle_24)
+                            .error(R.drawable.search_result)
+                            .into(img)
+                        val welcomeText = "Добро пожаловать, ${userAPI.name}!"
+                        findViewById<TextView>(R.id.textViewWelcome).text = welcomeText
+                    }
+                }
+            }
+
+        }
     }
 
-    fun onDriverClick(view: View) {
-        var intent = Intent(applicationContext, MainActivity::class.java)
-        intent.putExtra(Companion.ARG_USER_STATUS, "Driver")
-        startActivity(intent)
+    fun onEnterClick(view: View) {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        if (view.id == R.id.buttonDriver) {
+            intent.putExtra(ARG_USER_STATUS, "Driver")
+            startActivity(intent)
+        }
+        else {
+            intent.putExtra(ARG_USER_STATUS, "User")
+            startActivity(intent)
+        }
     }
 
-    fun onPassengerClick(view: View) {
-        var intent = Intent(applicationContext, MainActivity::class.java)
-        intent.putExtra(Companion.ARG_USER_STATUS, "User")
-        startActivity(intent)
-    }
-
-    fun onExit(view: View) {
-        var localDB = viewModel.getLocalDB(this)
+    fun onExit() {
+        val localDB = viewModel.getLocalDB(this)
         localDB.getDao().getAllUser().asLiveData().observe(this) {
             if (it.isNotEmpty()) {
                 for (user in it){
                     if (user.isInAcc) {
                         CoroutineScope(Dispatchers.IO).launch {
                             localDB.getDao().updateUserisAcc(
-                                "${user.id_user}",
+                                user.id_user,
                                false
                             )
                         }
-                        var intent = Intent(this, IntroActivity::class.java)
+                        val intent = Intent(this, IntroActivity::class.java)
                         startActivity(intent)
                     }
                 }
