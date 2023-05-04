c54fa764924c171af26944b4e56a118ece9db600
MNE4 <monashovnikolaye@gmail.com>
2023-05-04 20:22:39 +0300

    map debug

--- app/src/main/java/com/mne4/fromandto/CreateRequestActivity.kt
+++ app/src/main/java/com/mne4/fromandto/CreateRequestActivity.kt
@@ -215,13 +215,13 @@ class CreateRequestActivity : AppCompatActivity(), UserLocationObjectListener,
             var position = locationMapKit.cameraPosition()?.target
             if (position == null) {
                 Toast.makeText(applicationContext, "Ваше местоположение не обнаружено", Toast.LENGTH_SHORT).show()
-                finish()
-                var intent = Intent(applicationContext, CreateRequestActivity::class.java)
             }
-            mapView.map.move(CameraPosition(position!!, 17.0f, 0.0f, 0.0f),
-                Animation(Animation.Type.SMOOTH, 1f), null
-            )
-
+            else {
+                mapView.map.move(
+                    CameraPosition(position, 17.0f, 0.0f, 0.0f),
+                    Animation(Animation.Type.SMOOTH, 1f), null
+                )
+            }
         }
         catch (ex: Exception) {
             Log.d("moveToMyLocation","${ex.message}")
